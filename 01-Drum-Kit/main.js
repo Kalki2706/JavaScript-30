53d65bcbaa7851357fe717cff5833d16497177a9
Kalki <kalkithewarrior2706@proton.me>
2023-11-22 14:50:55 +0530

    Update main.js

--- 01-Drum-Kit/main.js
+++ 01-Drum-Kit/main.js
@@ -1,1 +1,37 @@
 'use strict';
+
+function playSound(key) {
+  const audio = document.querySelector(`audio[data-key=${key}]`);
+  const button = document.querySelector(`.--btn[data-key=${key}]`);
+
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+
+  // Sound will play on key-board key press
+  button.classList.add('playing');
+  setTimeout(() => {
+    button.classList.remove('playing');
+  }, 350);
+}
+
+const buttons = document.querySelectorAll('.--btn');
+
+// Sound will play on button click
+buttons.forEach((button) =>
+  button.addEventListener('click', function () {
+    const key = this.getAttribute('data-key');
+    playSound(key);
+
+    this.style.transform = 'scale(1.1)';
+
+    setTimeout(() => {
+      this.style.transform = 'scale(1)';
+    }, 350);
+  })
+);
+
+window.addEventListener('keydown', function (e) {
+  playSound(e.key);
+});
